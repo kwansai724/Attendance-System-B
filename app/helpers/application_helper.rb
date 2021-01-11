10e27b2591de744c86bccfd3b0f41bd45657bb66
kwansai724 <kwansai.taka724@gmail.com>
2021-01-11 02:21:28 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(page_name = "" )
+    bace_title = "AttendanceApp"
+    if page_name.empty?
+      bace_title
+    else
+      page_name + " | " + bace_title
+    end
+  end
 end
