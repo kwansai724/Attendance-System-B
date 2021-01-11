d06af45538f27657aae638139e0aa84741fab3cd
kwansai724 <kwansai.taka724@gmail.com>
2021-01-11 12:21:21 +0000

    第６章終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  
   get '/signup', to: 'users#new'
   root 'static_pages#top'
+  
+  # ログイン機能
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
