50699fab90a82e25b605aae29161109325dfde29
kwansai724 <kwansai.taka724@gmail.com>
2021-01-12 02:09:01 +0000

    第８章終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :updata]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :updata, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :current_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
   
   
   def index
@@ -38,6 +39,13 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+    
+  
   private
   
     def user_params
@@ -59,5 +67,9 @@ class UsersController < ApplicationController
     def correcut_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
